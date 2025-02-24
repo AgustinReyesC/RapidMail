1a97e03c2c7dc923818f3e6d36dbe8ac2d6a1a62
AgustinReyesC <gutyc@coffee>
2025-02-24 12:23:29 -0600

    CC y CCO

---
+++ src/rapidmail/RapidMail.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rapidmail;
+
+/**
+ *
+ * @author gutyc
+ */
+public class RapidMail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
