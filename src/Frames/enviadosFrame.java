9db1af9085813bfca0275cb0b5d7f304f6c19fed
Antonio de Jesús Muñoz Muñoz <allahuakbarr1230@gmail.com>
2025-02-23 22:43:09 -0600

    se añadió la cración de frame específico de cada mensaje, aún no está terminado ya que no muestra adjuntos

--- src/Frames/enviadosFrame.java
+++ src/Frames/enviadosFrame.java
@@ -6,11 +6,15 @@ package Frames;
 
 import java.awt.BorderLayout;
 import java.awt.ScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.Address;
 import javax.mail.BodyPart;
 import javax.mail.Message;
@@ -45,26 +49,21 @@ public class enviadosFrame extends javax.swing.JFrame {
          for (Message m : mensajes) {
 
              //creo el area
-            JTextArea currArea = new JTextArea(10,60);
-            
-            //le pongo los datos
-            try{
-                   //si el enviado tiene attachments
-                Multipart mp = (Multipart) m.getContent();
-
-                BodyPart bp = mp.getBodyPart(0);
-                
-            }catch(java.lang.ClassCastException ex){
-                //si no
-                String content = (String) m.getContent();
-                
-            }
+            JTextArea currArea = new JTextArea(1,60);
             
             //añado pequeña descripción del mensaje
             currArea.setVisible(true);
             //destinatario
-            currArea.setText("Para: " +InternetAddress.toString(m.getRecipients(Message.RecipientType.TO)));
-            
+            ArrayList<String> destinatariosParsed = new ArrayList();
+            for(String destinatario : InternetAddress.toString(m.getRecipients(Message.RecipientType.TO)).split(",")){
+                destinatariosParsed.add(destinatario);
+            }
+            if (destinatariosParsed.size() > 1){
+                currArea.setText("Para: " + destinatariosParsed.get(0) + " y " + (destinatariosParsed.size() - 1) + " más");
+            }else{
+                currArea.setText("Para: " + destinatariosParsed.get(0));
+            }
+                
             //asunto
              System.out.println("string length: " + currArea.getText().length());
             while(currArea.getText().length() < 33){//para que esté alineado
@@ -81,7 +80,35 @@ public class enviadosFrame extends javax.swing.JFrame {
             SimpleDateFormat formato = new SimpleDateFormat("d 'de' MMMM 'del' yyyy 'a las' hh:mm a", new Locale("es", "ES"));
             String fechaEnEspañol = formato.format(fecha);
             currArea.append("\t" + fechaEnEspañol);
-
+            
+            //le añado un listener para que cree el frame específico de cada mensaje
+            //código específico de como usar un mouse listener sacado de: https://www.youtube.com/watch?v=jptf1Wd_omw
+            currArea.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    //creo el otro frame y le doy la info de los mensajes
+                    Message currMensaje = mensajes[areasDeMensajes.indexOf(currArea)];
+                    String[] destinatarios = null;
+                    try {
+                        destinatarios = InternetAddress.toString(currMensaje.getAllRecipients()).split(",");
+                    } catch (MessagingException ex) {
+                        Logger.getLogger(enviadosFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    try {
+                        //codigo para checar si es texto sacado de: https://old.chuidiang.org/java/herramientas/javamail/leer-correo-javamail.php
+                        unCorreoEnviadoFrame frameDeCurrCorreo = new unCorreoEnviadoFrame(currMensaje.getSubject(), destinatarios,
+                                currMensaje.getContent(), currMensaje.isMimeType("text/*"));
+                        frameDeCurrCorreo.setVisible(true);
+                    } catch (MessagingException ex) {
+                        Logger.getLogger(enviadosFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(enviadosFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+            
+            currArea.setEditable(false);
+            
             //añado el panel
             panelEnviados.add(currArea);
             areasDeMensajes.add(currArea);
@@ -89,8 +116,6 @@ public class enviadosFrame extends javax.swing.JFrame {
         }catch(MessagingException ex){
            
         }
-        
-        System.out.println("cuantas Area:" + areasDeMensajes.size());
     }
     
     /**
@@ -102,6 +127,7 @@ public class enviadosFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        scrollPaneEnviados = new javax.swing.JScrollPane();
         panelEnviados = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -110,12 +136,14 @@ public class enviadosFrame extends javax.swing.JFrame {
         panelEnviados.setLayout(panelEnviadosLayout);
         panelEnviadosLayout.setHorizontalGroup(
             panelEnviadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 816, Short.MAX_VALUE)
+            .addGap(0, 824, Short.MAX_VALUE)
         );
         panelEnviadosLayout.setVerticalGroup(
             panelEnviadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 361, Short.MAX_VALUE)
+            .addGap(0, 371, Short.MAX_VALUE)
         );
+
+        scrollPaneEnviados.setViewportView(panelEnviados);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -123,15 +151,12 @@ public class enviadosFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(panelEnviados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addComponent(scrollPaneEnviados, javax.swing.GroupLayout.DEFAULT_SIZE, 826, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelEnviados, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(scrollPaneEnviados, javax.swing.GroupLayout.DEFAULT_SIZE, 373, Short.MAX_VALUE)
         );
 
         pack();
@@ -144,6 +169,7 @@ public class enviadosFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel panelEnviados;
+    private javax.swing.JScrollPane scrollPaneEnviados;
     // End of variables declaration//GEN-END:variables
 }
 
@@ -153,3 +179,4 @@ public class enviadosFrame extends javax.swing.JFrame {
 //System.out.println("Content: "+bp.getContent());
 //System.out.println("Content:" + content);
                 //creo el texArea de current message y lo añado
+ //System.out.println("TextArea " + (areasDeMensajes.indexOf(currArea) + 1) + " clicked!");
